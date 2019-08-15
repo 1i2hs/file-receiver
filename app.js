e812cbe6543559372320a0b6fa33f7bc61390ee0
Inho Han <inho.han@weather.com>
2019-08-16 01:34:41 +0900

    testing for file uploading listener

--- app.js
+++ app.js
@@ -36,28 +36,16 @@ const PORT = process.env.PORT || 3000;
 const APP_NAME = process.env.APP_NAME || "media-receiver";
 
 
-/**
+/**ㄴ
  * error handling
  */
 app.use((err, req, res, next) => {
-  logger.error("=================================================");
-  logger.error("time : " + new Date().toString());
-  logger.error("name : Exception");
-  logger.error("-------------------------------------------------");
   logger.error(err.stack);
-  logger.error("=================================================");
-  res.statusCode = 500;
-  res.send(err.stack);
+  res.status(500).send(err.stack);
 });
 
 process.on("uncaughtException", err => {
-  logger.error("\n\n");
-  logger.error("=================================================");
-  logger.error("time : " + new Date().toString());
-  logger.error("name : UncaughtException");
-  logger.error("-------------------------------------------------");
   logger.error(err.stack);
-  logger.error("=================================================\n\n");
 });
 
 app.listen(PORT, () => logger.info(`${APP_NAME} listening at port ${PORT}`));
@@ -65,8 +53,13 @@ app.listen(PORT, () => logger.info(`${APP_NAME} listening at port ${PORT}`));
 
 const mediaRouter = express.Router();
 mediaRouter.post("/video", upload.single("jyouth-video"), (req, res) => {
-  const file = req.file;
-  logger.info(`file: ${file.originalname} is being uploaded...`);
+  const { file } = req;
+  if(file) {
+    logger.info(`file: ${file.originalname} uploaded successfully.`);
+
+  } else {
+    logger.info(`file: ${file.originalname} is being uploaded...`);
+  }
 
   res.sendStatus(200);
 })
